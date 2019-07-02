67979c997df9317dc0f457690dcff8ab8d8f7452
KongBorup <avborup@gmail.com>
2019-07-02 19:09:00 +0200

    Add calendar sliding animations

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -9,12 +9,18 @@
         <img src="assets/icons/less-than.svg" alt="Næste måned" class="next-month-icon">
       </button>
     </header>
-    <CalendarGrid
-      :timestamp="currentTimestamp"
-      :activeDate="activeDate"
-      :key="currentTimestamp.toLocaleString()"
-      ref="calendarContent"
-    />
+    <transition name="slide" @before-enter="setSlidingClass">
+      <CalendarGrid
+        :timestamp="currentTimestamp"
+        :activeDate="activeDate"
+        :key="currentTimestamp.toLocaleString()"
+        ref="calendarContent"
+        :class="{
+          'slide-next': slideDir === 1,
+          'slide-prev': slideDir === -1,
+        }"
+      />
+    </transition>
   </div>
 </template>
 
@@ -49,16 +55,19 @@ export default {
       currentTimestamp: initialTimestamp,
       currentMonth: initialMonth,
       currentMonthName: initialMonthName,
+      slideDir: 1,
     };
   },
 
   methods: {
     incrementMonth() {
+      this.slideDir = 1;
       this.currentTimestamp = monthJump(this.currentTimestamp, 1);
       this.updateCalendar();
     },
 
     decrementMonth() {
+      this.slideDir = -1;
       this.currentTimestamp = monthJump(this.currentTimestamp, 1, -1);
       this.updateCalendar();
     },
@@ -69,15 +78,36 @@ export default {
 
       this.$refs.calendarContent.updateTimestamp(this.currentTimestamp);
     },
+
+    // Manually add and remove classes signifying which direction sliding should
+    // occur BEFORE the transition starts. Adding a binding attribute such as
+    // :class="{
+    //   'slide-next': slideDir === 1,
+    //   'slide-prev': slideDir === -1,
+    // }"
+    // to the CalendarGrid does not work alone, because the class is applied
+    // after the transition has completed, but it does work in conjunction with
+    // this method being called before the transition starts.
+    setSlidingClass() {
+      const classToAdd = this.slideDir === 1 ? 'slide-next' : 'slide-prev';
+      const classToRemove = this.slideDir === -1 ? 'slide-next' : 'slide-prev';
+
+      this.$refs.calendarContent.$el.classList.remove(classToRemove);
+      this.$refs.calendarContent.$el.classList.add(classToAdd);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+$header-height: 3rem;
+
 .calendar {
   display: grid;
   grid-template-rows: auto 1fr;
   height: 100%;
+  position: relative;
+  overflow-x: hidden;
 
   header {
     display: grid;
@@ -85,6 +115,7 @@ export default {
     padding: 0.5rem 2rem;
     align-items: center;
     justify-content: center;
+    height: $header-height;
 
     h2 {
       text-align: center;
@@ -109,7 +140,23 @@ export default {
         }
       }
     }
-
   }
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: transform 0.3s ease-in-out;
+  /* FIXME: Is this a good solution? */
+  position: absolute;
+  margin-top: $header-height;
+  width: 100%;
+  height: calc(100% - #{$header-height});
+}
+
+.slide-leave-to.slide-next, .slide-enter.slide-prev {
+  transform: translateX(-100%);
+}
+
+.slide-enter.slide-next, .slide-leave-to.slide-prev {
+  transform: translateX(100%);
 }
 </style>
