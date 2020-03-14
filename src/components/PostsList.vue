14dbd6a7b12655ad0d97c9297f62a9da11eaf5dd
Adrian Borup <avborup@gmail.com>
2020-03-14 10:06:25 +0100

    Split posts list into components

---
+++ src/components/PostsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="posts">
+    <PostsListItem
+      v-for="post in posts"
+      :key="post.id"
+      v-bind="post"
+      :forum="getForumFromId(post.forumId)"
+    />
+  </ul>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import PostsListItem from '@/components/PostsListItem.vue';
+
+export default {
+  name: 'PostsList',
+  components: {
+    PostsListItem,
+  },
+
+  computed: {
+    ...mapGetters({
+      posts: 'getCurrentForumPosts',
+      getForumFromId: 'getForumFromId',
+    }),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.posts {
+  list-style-type: none;
+}
+</style>
--- src/views/Forum.vue
+++ src/views/Forum.vue
@@ -2,59 +2,18 @@
   <div class="forum-content-wrapper">
     <div class="forum">
       <h1>Opslag</h1>
-      <ul class="posts">
-        <li v-for="post in posts" :key="post.id">
-          <div class="avatar">
-            <img src="/assets/icons/person.svg" alt="Avatar icon">
-          </div>
-          <h4 :title="post.title">{{ post.title }}</h4>
-          <p class="author-date">
-            <span class="author">{{ post.author }}</span>
-            <span class="date">
-              <span class="sep"> &ndash;</span>
-              {{ toElapsedTimeStr(post.date) }}
-            </span>
-          </p>
-          <div class="post-info">
-            <div class="post-comment-info" :title="`${post.commentCount} kommentarer`">
-              <img src="/assets/icons/comment.svg" alt="Comment icon">
-              <span class="post-num-comments">{{ post.commentCount }}</span>
-            </div>
-          </div>
-          <div
-            class="post-forum"
-            :title="getForumFromId(post.forumId).name"
-            :style="{
-              borderColor: getForumFromId(post.forumId).color,
-              color: getForumFromId(post.forumId).color,
-            }"
-          >
-            {{ getForumFromId(post.forumId).name }}
-          </div>
-        </li>
-      </ul>
+      <PostsList />
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-// FIXME: ESLint complains that this function does not exist.
-// eslint-disable-next-line
-import { toElapsedTimeStr } from '@/dateUtils';
+import PostsList from '@/components/PostsList.vue';
 
 export default {
   name: 'Forum',
-
-  computed: {
-    ...mapGetters({
-      posts: 'getCurrentForumPosts',
-      getForumFromId: 'getForumFromId',
-    }),
-  },
-
-  methods: {
-    toElapsedTimeStr,
+  components: {
+    PostsList,
   },
 
   beforeMount() {
@@ -85,135 +44,6 @@ export default {
 h1 {
   font-size: 1.6rem;
   margin-bottom: 1rem;
-}
-
-.posts {
-  list-style-type: none;
-
-  li {
-    display: grid;
-    grid-template-columns: 3rem 1fr 3rem 6rem;
-    grid-template-rows: 1fr 1fr;
-    grid-template-areas:
-      "avatar title       info forum"
-      "avatar author-date info forum";
-    column-gap: 1rem;
-    align-items: center;
-    padding: 0.75rem 0.5rem;
-    border-radius: 0.5rem;
-    border: 0.1rem solid rgba(0, 0, 0, 0.05);
-
-    &:not(:last-child) {
-      margin-bottom: 1rem;
-    }
-
-    &:hover {
-      background-color: rgba(0, 0, 0, 0.03);
-    }
-
-    .avatar {
-      width: 3rem;
-      height: 3rem;
-      grid-area: avatar;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: $primary-accent;
-      border-radius: 100%;
-
-      img {
-        width: 1.5rem;
-        height: 1.5rem;
-      }
-    }
-
-    h4 {
-      grid-area: title;
-      font-size: 1rem;
-      font-weight: bold;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-
-    .author-date {
-      grid-area: author-date;
-
-      .author {
-        text-transform: uppercase;
-        color: $primary-accent;
-        font-weight: bold;
-        font-size: 0.9rem;
-      }
-
-      .date {
-        color: rgba(0, 0, 0, 0.6);
-      }
-    }
-
-    .post-info {
-      grid-area: info;
-
-      .post-comment-info {
-        display: flex;
-        align-items: center;
-        font-size: 0.9rem;
-        color: rgba(0, 0, 0, 0.6);
-
-        img {
-          height: 1rem;
-          margin-right: 0.5rem;
-        }
-      }
-    }
-
-    .post-forum {
-      grid-area: forum;
-      border-radius: 10rem;
-      border-style: solid;
-      border-width: 0.1rem;
-      text-align: center;
-      padding: 0.4rem 0.7rem;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      font-size: 0.7rem;
-      text-transform: uppercase;
-      font-weight: bold;
-      color: $primary-accent;
-    }
-  }
-}
-
-@media (max-width: 600px) {
-  .posts li {
-    grid-template-rows: auto auto;
-    grid-template-columns: 2rem 1fr 2.5rem 4rem;
-    grid-template-areas:
-      "avatar author-date info  forum"
-      "title  title  title title";
-    column-gap: 0.5rem;
-    row-gap: 0.5rem;
-
-    .avatar {
-      width: 2rem;
-      height: 2rem;
-
-      img {
-        width: 1rem;
-        height: 1rem;
-      }
-    }
-
-    .author-date {
-      display: flex;
-      flex-direction: column;
-
-      .sep {
-        display: none;
-      }
-    }
-  }
 }
 
 @media (min-width: 900px) {
