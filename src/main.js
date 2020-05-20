f48a0b4c3e62c1b36690fad225c9d89fb9529cfb
Adrian Borup <avborup@gmail.com>
2020-05-20 12:44:44 +0200

    Add event bubbling plugin

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ Vue.config.productionTip = false;
 
 Vue.use(Editor);
 
+// This provides a $bubble method that emits an event on ALL parent components.
+Vue.use((vue) => {
+  // eslint-disable-next-line no-param-reassign
+  vue.prototype.$bubble = function $bubble(eventName, ...args) {
+    let component = this;
+    do {
+      component.$emit(eventName, ...args);
+      component = component.$parent;
+    } while (component);
+  };
+});
+
 new Vue({
   router,
   store,
