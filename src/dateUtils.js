avborup/rollespilsfabrikken-forum-calendar-frontend

commit a136f00e18a54b5d9aac5710c2e428c9957b4871

Fix calendar not working in Edge
In Edge, passing an array to the Date constructor does not work. This
commit fixes that by spreading the array such that the contents are
passed as separate arguments.

Fixes: #18

src/dateUtils.js
@@ -33,7 +33,7 @@ export function monthJump(oldDate, amount, dir = 1) {
 // Returns 0-indexed day of the week, Monday-Sunday
 export function getWeekday(...args) {
   // FIXME: Why does the date go one month back by default?
-  const date = monthJump(new Date(args), 1);
+  const date = monthJump(new Date(...args), 1);
   let weekDay = date.getDay() - 1;
 
   if (weekDay === -1) {

commit 8c11613789e1dded0ad752ad85002d7f81d953ae

Fix weekday on date being offset by a month
Fixing #18 with #a136f00 introduced a new bug. Passing args directly to
the Date constructor (e.g. without spreading it) resulted in a month
offset by 1, and it also broke the calendar in Edge. "Fixing" the issue
by spreading the args also fixed the month offset, but this was not
accounted for. This commit remedies that.

Fixes: #29

src/dateUtils.js
@@ -32,8 +32,7 @@ export function monthJump(oldDate, amount, dir = 1) {
 
 // Returns 0-indexed day of the week, Monday-Sunday
 export function getWeekday(...args) {
-  // FIXME: Why does the date go one month back by default?
-  const date = monthJump(new Date(...args), 1);
+  const date = new Date(...args);
   let weekDay = date.getDay() - 1;
 
   if (weekDay === -1) {